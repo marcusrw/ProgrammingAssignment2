f62ac243380ca53bb6133caaae02e09d06cfa1be
Marcus Wilson <marcusrw@gmail.com>
2014-11-23 16:25:41 -0700

    Implemented makeCacheMatrix and cacheSolve methods.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix is a wrapper around the matrix mat, that allows for
+## storage and access of the inverse of mat.
+## Contains get/set methods for both the original matrix and its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(mat = matrix()) {
+        matInverse <- NULL  ## storage for the inverse of mat
+        
+        
+        setMatrix <- function(newMatrix) {
+                mat <<- newMatrix
+                matInverse <<- NULL
+        }
+        
+        getMatrix <- function() {
+                mat
+        }
+        
+        setInverse <- function(mInverse) {
+                matInverse <<- mInverse
+        }
+        
+        getInverse <- function() {
+                matInverse
+        }
+        
+        list(setMatrix = setMatrix, getMatrix = getMatrix,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve computes and stores the inverse of the matrix mat 
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(cacheMatrix, ...) {
+        
+        matInverse <- cacheMatrix$getInverse()
+        if (!is.null(matInverse)){
+                message("Inverse is already computed.  Returning cached copy.")
+                return(matInverse)
+        }
+        mat <- cacheMatrix$getMatrix()
+        matInverse <- solve(mat)
+        cacheMatrix$setInverse(matInverse)
+        
+        matInverse
+        
+        ## Return a matrix that is the inverse of 'mat'
+        
 }
